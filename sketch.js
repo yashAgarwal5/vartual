203c75e0b4ec4cba9a1530fc88a769fca6ae74b9
yashAgarwal5 <72796286+yashAgarwal5@users.noreply.github.com>
2021-01-20 00:07:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var dog,happyDog,database,foodS,foodStock;
+var dogImg;
+var happyImg;
+
+function preload()
+{
+  dogImg=loadImage("images/doglmg.png");
+  happpyImg=loadImage("images/doglmg1.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  database = firebase.database();
+  
+dog=createSprite(250,250,20,20);
+dog.addImage(dogImg);
+
+foodStock=database.ref('Food');
+foodStock.on("value",readStock);
+
+}
+
+
+function draw() {  
+  background(46,139,87)
+
+  if (keyWentDown(UP_ARROW)){
+    writeSock(foodS);
+  }
+
+  drawSprites();
+
+  textSize(20);
+  fill("lightyellow")
+  text('Note:press UP_ARROW key To feed drago milk',50,40);
+  fill("lightyellow")
+}
+
+function readStock(data){
+foodS=data.val();
+}
+
+function writeSock(){
+  if(x<=0){
+    x=0;
+  }
+  else{
+    x=x-1;
+  }
+
+database.ref('/').update({
+  Food:x
+})
+}
